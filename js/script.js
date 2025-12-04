c89ca378c7a12f866db1d03f03f433a6fa22cedf
madhuri <madhurikarthik15@gmail.com>
2025-12-04 11:10:45 +0530

    fixed menu and added view cart

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 function addToCart(productName){ alert(productName + " has been added to cart!"); }
+
+function addToCart(name, price, img) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({ name, price, img });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " added to cart!");
+}
